aeaec49ccfdad5a9b88f24dee78f421991dd2c2a
JordanClem <31521143+JordanClem@users.noreply.github.com>
2017-09-23 16:07:51 -0400

    added delete test

--- tests/task/test_delete_task.py
+++ tests/task/test_delete_task.py
@@ -20,6 +20,14 @@ class TestDeleteTask(BaseTestCase):
             Task.query.filter_by(id=received_id).first()
         )
 
-    # def test_delete_non_exisiting(self):
-        # assertEquals
-        # received[0]["args"][0]["error"], "Task does not exist"
+    def test_delete_non_existing(self):
+
+        self.client.get_received()
+        self.client.emit("delete_task", {"id": 1})
+        received = self.client.get_received()
+
+        received_id = received[0]["args"][0]["error"]
+
+        self.assertEquals(
+            received_id, "Task does not exit."
+        )
