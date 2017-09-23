f24c62a78b88b6f26dcaad4e738aa59add9ec534
Stephen James <stephenorjames@gmail.com>
2017-09-23 03:00:29 -0400

    Add basic SQLAlchemy setup

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+@app.route("/")
 def test():
     return "test"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
