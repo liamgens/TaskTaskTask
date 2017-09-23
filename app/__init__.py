10edb2437f8b18ddb66f5e4d8a82aa8b672fb8f8
Stephen James <stephenorjames@gmail.com>
2017-09-23 09:12:30 -0400

    Add Flask-Script

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../database.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 db = SQLAlchemy(app)
+
 
 from app.views import task_list
 from app.views import task
--- manage.py
+++ manage.py
@@ -1,6 +1,16 @@
+from flask_script import Manager
+
 from app import app, db
+
+
+manager = Manager(app)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db)
 
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    manager.run()
