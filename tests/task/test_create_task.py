eda004d30ff01b6b969ff09cbc7d3daebc0b00c6
JordanClem <31521143+JordanClem@users.noreply.github.com>
2017-09-23 16:53:43 -0400

    added create task tests

--- tests/task/test_create_task.py
+++ tests/task/test_create_task.py
@@ -25,3 +25,31 @@ class TestCreateTask(BaseTestCase):
                 "parent_id": None,
             }]
         )
+
+    def test_no_title(self):
+        task = Task(None, "yeet", 1)
+        task = task.as_json
+
+        self.client.get_received()
+        self.client.emit("create_task", task)
+        received = self.client.get_received()
+
+        received_title = received[0]["args"][0]["error"]
+
+        self.assertEquals(
+            received_title, "Tasks must have a title."
+        )
+
+    def test_no_id(self):
+        task = Task("yo", "yeet", None)
+        task = task.as_json
+
+        self.client.get_received()
+        self.client.emit("create_task", task)
+        received = self.client.get_received()
+
+        received_id = received[0]["args"][0]["error"]
+
+        self.assertEquals(
+            received_id, "Tasks must have a parent Task List."
+        )
