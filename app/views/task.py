ee1d8ea6ba70e8851d7c71e466904c2959ff5c7b
Liam Gensel <liamgens@gmail.com>
2017-09-23 21:31:06 -0400

    added dummy data socket function

--- app/views/task.py
+++ app/views/task.py
@@ -3,7 +3,9 @@ from flask_socketio import emit
 from app.models import Task
 import json
 
-#method to create tasks - gives error if no title or id
+# method to create tasks - gives error if no title or id
+
+
 @socketio.on("create_task")
 def create_task(data):
     title = data.get("title")
@@ -20,7 +22,9 @@ def create_task(data):
         db.session.commit()
         emit("create_task", new_task.as_json, broadcast=True)
 
-#method that takes a change in a task and updates it to all devices - gives error if there is no task to update
+# method that takes a change in a task and updates it to all devices - gives error if there is no task to update
+
+
 @socketio.on("update_task")
 def update_task(data):
     task = Task.query.filter_by(id=data["id"]).first()
@@ -35,7 +39,9 @@ def update_task(data):
         db.session.commit()
         emit("update_task", response, broadcast=True)
 
-#method to delete tasks - gives error if no task to delete
+# method to delete tasks - gives error if no task to delete
+
+
 @socketio.on("delete_task")
 def delete_task(data):
     old_task = Task.query.filter_by(id=data["id"]).first()
@@ -48,3 +54,57 @@ def delete_task(data):
             "id": data["id"],
         }
         emit("delete_task", data, broadcast=True)
+
+
+@socketio.on("dummy_data")
+def dummy_data(data):
+    task_list =
+    {
+        1: [1, 2, 3],
+        2: [4, 5],
+        3: [6, 7],
+    }
+
+    tasks = {
+        1: {
+            description: "Whaddup up son",
+            isCompleted: false,
+            listId: 1,
+            sublistId: 2,
+        },
+        2: {
+            description: "It's yo dad",
+            isCompleted: false,
+            listId: 1,
+        },
+        3: {
+            description: "Never talk to my son again",
+            isCompleted: true,
+            listId: 1,
+        },
+        4: {
+            description: "Fix tasktasktask's backend because Liam got so many opinions",
+            isCompleted: true,
+            listId: 2,
+        },
+        5: {
+            description: "Memes to Short",
+            isCompleted: false,
+            listId: 2,
+            sublistId: 3,
+        },
+        6: {
+            description: "Windows XP Dialog",
+            isCompleted: false,
+            listId: 3,
+        },
+        7: {
+            description: "Cost of iPhone X",
+            isCompleted: true,
+            listId: 3,
+        },
+    }
+
+    dummy = {task_list, tasks}
+
+    emit("dummy_data", dummy)
