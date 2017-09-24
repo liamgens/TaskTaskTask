b9606f0118ca89dfc700e485c7fe42c7b1ae2181
Angus Lam <angushtlam@gmail.com>
2017-09-24 03:04:43 -0400

    Fixed checkbox messing with description

--- frontend/components/task/index.js
+++ frontend/components/task/index.js
@@ -13,6 +13,7 @@ export default class Task extends React.Component {
     this.state = {
       isEditing: false,
       description: this.props.description,
+      originalDescription: this.props.description,
     }
 
     this.finishEditingTask = this.finishEditingTask.bind(this)
@@ -25,24 +26,32 @@ export default class Task extends React.Component {
   }
 
   finishEditingTask() {
-    this.setState({ isEditing: false })
-    updateTask(this.props.id, this.state.description, this.state.isCompleted)
+    if (this.state.originalDescription !== this.state.description) {
+      updateTask(this.props.id, this.state.description, this.props.isCompleted)
+    }
+
+    this.setState({ isEditing: false, })
   }
 
   handleBlur(event) {
-    updateTask(this.props.id, this.state.description, this.state.isCompleted)
+    if (this.state.isEditing) this.finishEditingTask()
   }
 
   handleCheckbox(event) {
+    if (this.state.isEditing) this.finishEditingTask()
     updateTask(this.props.id, this.state.description, !this.props.isCompleted)
   }
 
   handleEnter(event) {
-    if (this.state.description != event.key === 'Enter') this.finishEditingTask()
+    if (event.key === 'Enter') this.finishEditingTask()
   }
 
   handleFocus(event) {
-    console.log('focused')
+    this.setState({
+      description: this.props.description,
+      originalDescription: this.props.description,
+      isEditing: true,
+    })
   }
 
   handleInput(event) {
@@ -56,23 +65,27 @@ export default class Task extends React.Component {
   render() {
     return (
       <div className="component__task">
-        <input type="checkbox" checked={ this.props.isCompleted || false } onChange={ this.handleCheckbox } />
-        <input type="text"
-               onBlur={ this.handleBlur }
-               onChange={ this.handleInput }
-               onFocus={ this.handleFocus }
-               onKeyPress={ this.handleEnter }
-               value={ this.props.description } />
-        {
-          this.props.sublistId ?
-          <TaskList listId={ this.props.sublistId } /> : (
-            <div className="add_sublist" onClick={ this.handleAddSublist }>
-              <Icon name="add" description="add" style={
-                { height: '1rem', width: '1rem', }
-              } />
-            </div>
-          )
-        }
+        <div className="wrapper">
+          <input type="checkbox"
+                 checked={ this.props.isCompleted || false }
+                 onChange={ this.handleCheckbox } />
+          <input type="text"
+                onBlur={ this.handleBlur }
+                onChange={ this.handleInput }
+                onFocus={ this.handleFocus }
+                onKeyPress={ this.handleEnter }
+                value={ this.state.isEditing ? this.state.description : this.props.description } />
+          {
+            this.props.sublistId ?
+            <TaskList listId={ this.props.sublistId } /> : (
+              <div className="add_sublist" onClick={ this.handleAddSublist }>
+                <Icon name="add" description="add" style={
+                  { height: '1rem', width: '1rem', }
+                } />
+              </div>
+            )
+          }
+        </div>
       </div>
     )
   }
