938fef41c81efd67411f4c4dddc3eb9eabb69b8c
Stephen James <stephenorjames@gmail.com>
2017-09-24 02:08:46 -0400

    Add spport for db URI from env

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
+import os
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../database.sqlite3"
+app.config["DEBUG"] = os.getenv("DATABASE_URI") is None
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "sqlite:///../database.sqlite3")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
