095f45465875ac84aa64f444c27a14ca193f9dda
Liam Gensel <liamgens@gmail.com>
2017-09-23 23:55:33 -0400

    added sublist tests - need to delete lists though

--- tests/test_remove_task.py
+++ tests/test_remove_task.py
@@ -5,16 +5,7 @@ from app.models import Task, TaskList
 
 class TestRemoveTask(BaseTestCase):
     def test_task_id_int(self):
-        data = {"task_id": "1"}
-        self.client.get_received()
-        self.client.emit("remove_task", data)
-        received = self.client.get_received()
-        self.assertEqual(
-            received[0]["args"][0]["error"], "remove_task with invalid task_id (type)"
-        )
-
-    def test_task_id_invalid(self):
-        data = {"task_id": 2}
+        data = {"id": "1"}
         self.client.get_received()
         self.client.emit("remove_task", data)
         received = self.client.get_received()
@@ -22,8 +13,18 @@ class TestRemoveTask(BaseTestCase):
             received[0]["args"][0]["error"], "remove_task with invalid task_id"
         )
 
+    def test_task_id_invalid(self):
+        data = {"id": 3}
+        self.client.get_received()
+        self.client.emit("remove_task", data)
+        received = self.client.get_received()
+        self.assertEqual(
+            received[0]["args"][0]["error"], "remove_task with task_id of non-existent object"
+        )
+
     def test_remove_task_wo_sublist(self):
-        task = Task(description="test", list_id=1)
+        task = Task(description="test", list_id=2)
+
         self.db.session.add(task)
         self.db.session.commit()
 
@@ -32,15 +33,51 @@ class TestRemoveTask(BaseTestCase):
         )
 
         data = task.as_json()
-        print (data)
         self.client.get_received()
         self.client.emit("remove_task", data)
         received = self.client.get_received()
-        print(received)
         received_id = received[0]["args"][0]["id"]
         self.assertIsNone(
             Task.query.filter_by(id=received_id).first()
         )
 
     def test_remove_task_with_sublist(self):
-        pass
+        mainlist = TaskList()
+        sublist = TaskList()
+        self.db.session.add(mainlist)
+        self.db.session.add(sublist)
+        self.db.session.commit()
+
+        task = Task(description="mainlisttask", list_id=1, sublist_id=2)
+        task1 = Task(description="sublisttask", list_id=2)
+
+        self.db.session.add(task)
+        self.db.session.add(task1)
+        self.db.session.commit()
+
+        self.assertIsNotNone(
+            Task.query.filter_by(id=1).first()
+        )
+
+        self.assertIsNotNone(
+            Task.query.filter_by(id=2).first()
+        )
+        data = task.as_json()
+        self.client.get_received()
+        self.client.emit("remove_task", data)
+
+        self.assertIsNone(
+            Task.query.filter_by(id=1).first()
+        )
+
+        self.assertIsNone(
+            Task.query.filter_by(id=2).first()
+        )
+
+        self.assertIsNotNone(
+            TaskList.query.filter_by(id=1).first()
+        )
+
+        self.assertIsNone(
+            TaskList.query.filter_by(id=2).first()
+        )
